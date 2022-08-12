8f4be48b453db424bf7ac2de0a131c032872d264
Mortuus <mortuus.testing@gmail.com>
2022-08-12 09:07:47 +0700

    Fix typo

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.use(express.urlencoded({extended: false}));
 
 app.get("/", (req, res) => {
-	res.send("What are you doing?"
+	res.send("What are you doing?")
 });
 app.get("/api", (req, res) => {
 	res.send("Welcome to Express API")	
