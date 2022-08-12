3d18d25227ea5646bdbc892492961cf9ec616dc6
Mortuus <mortuus.testing@gmail.com>
2022-08-12 10:11:43 +0700

    Add styling

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 const express  = require('express');
+const path = require('path')
 const app = express();
 const port = 3000;
 
 app.use(express.urlencoded({extended: false}));
+// Static needed for fetching static object like image
+app.use(express.static('public'))
 
+// Default route
 app.get("/", (req, res) => {
-	res.send("What are you doing?")
+	res.sendFile('index.html', {root: path.join(__dirname, 'public')});
 });
+
+// API Route
 app.get("/api", (req, res) => {
-	res.send("Welcome to Express API")	
+	try {
+		res.json({
+			status: 200,
+			messege: "Success!"
+	})
+	}
+	catch(error) {
+		console.log(error)
+		res.status(500).send("Server error!")
+	}
 });
 app.get("/api/users", (req, res) => {
 	res.json([
-		{ id: 1, name: "Akahashi", password: "Pass"},
-		{ id: 2, name: "Haruka", password: "Word"}
+		{ id: 1, name: "Takahashi", password: "takahashi"},
+		{ id: 2, name: "Haruka", password: "haruka"},
+		{ id: 3, name: "Tomoe", password: "tomoe"}
 	])
 });
 
